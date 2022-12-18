a73f9a8bb90702a7c2c5cf6abced466b2d5d3949
yinli1225 <yinli@uw.edu>
2022-12-18 08:52:34 -0800

    week8

--- cli wk8.py
+++ cli wk8.py
@@ -12,26 +12,26 @@ if __name__ == "__main__":
             human_name = input("Name of the player: ")
             while human_name.lower() == "bot":
                 human_name = input("Invalid name, pick another name: ")
-            player1 = Human(human_name)  
+            player1 = Human(human_name,"Human")  
         else:
             bot_name = "Bot" 
-            player1 = Bot(bot_name)
+            player1 = Bot(bot_name,"Bot")
 
         determine_player_2 = int(input("Choose second player, 0 for a human, 1 for a bot:"))
         if determine_player_2 == 0:
             human_name = input("Name of the player: ")
             while human_name == player1.name or human_name.lower() == "bot":
                 human_name = input("Invalid name, pick another name: ")
-            player2 = Human(human_name)
+            player2 = Human(human_name,"Human")
         else:
             bot_name = "Bot2"
-            player2 = Bot(bot_name)
+            player2 = Bot(bot_name,"Bot")
            
 
         print(player1.name, "vs", player2.name)
         game = Game(player1, player2)
         winner = game.run()
-        print(winner, " won the game")
+        print("The winner is:", winner)
 
         end_game = input("End game or not, y for end, n for continue: ")
 
