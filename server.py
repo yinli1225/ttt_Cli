4cc9ec1ba6dbd4b150b4f96afa5d632331bc2ca2
yinli1225 <yinli@uw.edu>
2022-12-19 09:33:26 -0800

    server

--- server.py
+++ server.py
@@ -1,14 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from logic_wk8 import Board, Bot, Game, Human
+
 app = Flask(__name__)
 
-@app.get('/')
-def index():
-    return '''
-    <a href="resume">Resume</a>
-    -
-    <a href="contact">Contact</a>
-    '''
-    
-@app.get('/resume')
-def resume():
-    return 'Lorem ipsum etc'
+
+
+# @app.route('/')
+# def mode():
+#     player1 = Game("Player 1", "human")
+#     player2 = Game("Player 2", "human")
+#     game = Game(player1, player2)
+#     winner = game.run()
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run()
+
+
+# @app.route('/')
+# def play():
+#     player1 = Game("Player 1", "human")
+#     player2 = Game("Player 2", "human")
+#     game = Game(player1, player2)
+#     winner = game.run()
+#     return render_template('play.html')
+
+# if __name__ == '__main__':
+#     app.run()
+   
+
+@app.route('/')
+def stats():
+    # player1 = Game("Player 1", "human")
+    # player2 = Game("Player 2", "human")
+    # game = Game(player1, player2)
+    # winner = game.run()
+    return render_template('stats.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
